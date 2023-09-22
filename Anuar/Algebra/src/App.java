b2a5aa0c6ec61c21c793beacc03c6399a4ce22a5
AllanUrbina <142625851+AllanUrbina@users.noreply.github.com>
2023-09-22 14:17:21 -0600

    Add files via upload

---
+++ Anuar/Algebra/src/App.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class App {
+    
+    
+     
+     public static void main (String [] args){
+        Scanner lector= new Scanner(System.in);
+       
+      byte elegir;
+       System.out.println("Se le dara un menu de opciones ");
+      
+       System.out.println("1) Verificar si 2 Matrices son iguales ");
+       
+       System.out.println("2) Sumar 2 Matrices");
+      
+      System.out.println("3) Desea Generar una Matrix nilpotente");
+       
+      System.out.println("4) Obtener el inverso aditivo de una matriz");
+       
+      System.out.println("5) obtener una Matriz ");
+   
+      System.out.println("6");
+   
+       elegir = lector.nextByte();
+   
+      switch (elegir) {
+   
+       case 1:
+       
+   
+   
+       break;
+   
+       default:
+      
+       break;
+   
+       
+      }
+    }
+}
+
